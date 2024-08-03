0bb2cc87de0e8c9bba3d02d76f10c20ec3a35099
Mohit <mohitkdm21@gmail.com>
2024-08-03 13:34:28 +0530

    Clear Clutter

--- Clutter Clear/index.js
+++ Clutter Clear/index.js
@@ -1,45 +1,25 @@
-const fs = require('fs');
-const path = require('path');
 
-const directoryPath = './your-directory'; // Replace with your directory path
+import fs from "fs/promises"
+import fsn from "fs"
+import path from "path";
 
-function organizeFiles(dirPath) {
-  fs.readdir(dirPath, (err, files) => {
-    if (err) {
-      console.error(`Error reading directory: ${err.message}`);
-      return;
+const basepath = "D:\\Mohit Personal Notes c,python,htnl\\Sigma CSS Web development With Harry Code\\Day 76 to 100\\Day 93 Exercise 15 - Solution & Shoutouts"
+
+let files = await fs.readdir(basepath)
+
+for (const item of files) {
+    console.log("Running For ",item);
+    let ext = item.split(".")[item.split(".").length - 1]
+
+    if (ext != "js" && ext != "json" && item.split(".").length > 1){
+
+        if (fsn.existsSync(path.join(basepath, ext))) {
+            // Move the File to this directory if it is not a js and json file 
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
+        else {
+            fs.mkdir(ext)
+            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
+        }
     }
-
-    files.forEach((file) => {
-      const filePath = path.join(dirPath, file);
-      fs.stat(filePath, (err, stats) => {
-        if (err) {
-          console.error(`Error getting stats of file: ${err.message}`);
-          return;
-        }
-
-        if (stats.isFile()) {
-          const fileExt = path.extname(file).toLowerCase().slice(1);
-          const folderName = fileExt;
-
-          const destinationDir = path.join(dirPath, folderName);
-          const destinationPath = path.join(destinationDir, file);
-
-          if (!fs.existsSync(destinationDir)) {
-            fs.mkdirSync(destinationDir);
-          }
-
-          fs.rename(filePath, destinationPath, (err) => {
-            if (err) {
-              console.error(`Error moving file: ${err.message}`);
-            } else {
-              console.log(`Moved ${file} to ${folderName}`);
-            }
-          });
-        }
-      });
-    });
-  });
 }
-
-organizeFiles(directoryPath);
